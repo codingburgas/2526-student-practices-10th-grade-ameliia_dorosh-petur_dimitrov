64f22be18e01e9e5c2dc9c14adbb801b931fa733
PADimitrov23 <PADimitrov23@codingburgas.bg>
2026-04-09 18:24:52 +0300

    Define DB class architecture

--- kino-plex/database/database.cpp
+++ kino-plex/database/database.cpp
@@ -1,5 +1,47 @@
 #include "database.h"
 
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QCryptographicHash>
+#include <QVariant>
+#include <QCoreApplication>
+
 Database::Database(QWidget *parent)
     : QWidget{parent}
 {}
+
+Database::~Database(){}
+
+Database& Database::createInstance(){
+    static Database databaseInstance;
+    return databaseInstance;
+}
+
+bool Database::initialize(){
+    // open DB file and set filepath
+    return true;
+}
+
+bool Database::createUsersTable(){
+    // CREATE TABLE IF NOT EXISTS users (...)
+    return true;
+}
+
+QString Database::hashPassword(const QString& plainPass) const{
+    // return sha256 hex string
+    return QString();
+}
+
+bool Database::registerUser(const QString& username, const QString& plainPass){
+    // prepare INSERT query and bind values
+    return true;
+}
+
+bool Database::validateLogin(const QString& username, const QString& plainPass){
+    // select stored hash and compare
+    return false;
+}
+
+void Database::close(){
+    // close connection if open
+}
--- kino-plex/database/database.h
+++ kino-plex/database/database.h
@@ -6,7 +6,17 @@ class Database : public QWidget
 {
     Q_OBJECT
 public:
+    static Database& createInstance();
+    bool initialize();
+    bool createUsersTable();
+    QString hashPassword(const QString& plainPass) const;
+    bool registerUser(const QString& username, const QString& plainPass);
+    bool validateLogin(const QString& username, const QString& plainPass);
+    void close();
+
+private:
     explicit Database(QWidget *parent = nullptr);
+    ~Database();
 
 signals:
 };
