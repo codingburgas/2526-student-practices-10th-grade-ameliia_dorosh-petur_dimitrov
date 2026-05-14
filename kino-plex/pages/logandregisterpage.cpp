b8eaa77fb87f823b21c7e3094a949073e7c5e616
PADimitrov23 <PADimitrov23@codingburgas.bg>
2026-05-14 15:31:11 +0300

    Add code formatting to logAndRegister page

--- kino-plex/pages/logandregisterpage.cpp
+++ kino-plex/pages/logandregisterpage.cpp
@@ -1,7 +1,42 @@
 #include "logandregisterpage.h"
 
-#include <QWidget>
+#include "logandregisterpage.h"
 
+<<<<<<< HEAD
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLabel>
+#include <QStackedWidget>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QFormLayout>
+
+LogAndRegisterPage::LogAndRegisterPage(QWidget *parent)
+	: QWidget(parent),
+      signInBtn(nullptr), registerBtn(nullptr), stack(nullptr),
+      loginUser(nullptr), loginPass(nullptr), loginSubmit(nullptr), loginError(nullptr),
+      regUser(nullptr), regPass(nullptr), regPassRepeat(nullptr), regSubmit(nullptr), regError(nullptr)
+{
+	createTopBar();
+	createLoginPage();
+	createRegisterPage();
+
+	QVBoxLayout *root = new QVBoxLayout(this);
+	QHBoxLayout *topBar = new QHBoxLayout;
+    topBar->addWidget(signInBtn);
+    topBar->addWidget(registerBtn);
+	topBar->addStretch();
+
+	root->addLayout(topBar);
+
+    stack = new QStackedWidget(this);
+    root->addWidget(stack, 1);
+
+	setLayout(root);
+
+	// default to sign in
+    stack->setCurrentIndex(0);
+=======
 #include <QTabWidget>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
@@ -78,12 +113,118 @@ LogAndRegisterPage::LogAndRegisterPage(QWidget *parent) : QWidget(parent)
     // connections
     connect(loginButton, &QPushButton::clicked, this, &LogAndRegisterPage::onLoginClicked);
     connect(registerButton, &QPushButton::clicked, this, &LogAndRegisterPage::onRegisterClicked);
+>>>>>>> 5cb0458f0d06901e4cf4ee5770282626e17c14db
 }
 
 LogAndRegisterPage::~LogAndRegisterPage()
 {
 }
 
+<<<<<<< HEAD
+void LogAndRegisterPage::createTopBar()
+{
+    signInBtn = new QPushButton(tr("Sign In"), this);
+    registerBtn = new QPushButton(tr("Register"), this);
+
+    signInBtn->setObjectName("navButton");
+    registerBtn->setObjectName("navButton");
+
+    connect(signInBtn, &QPushButton::clicked, this, [this]() {
+        if (stack) stack->setCurrentIndex(0);
+	});
+
+    connect(registerBtn, &QPushButton::clicked, this, [this]() {
+        if (stack) stack->setCurrentIndex(1);
+	});
+}
+
+void LogAndRegisterPage::createLoginPage()
+{
+	QWidget *page = new QWidget(this);
+	QVBoxLayout *v = new QVBoxLayout(page);
+
+    loginError = new QLabel(page);
+    loginError->setStyleSheet("color: #e11b22");
+    loginError->setWordWrap(true);
+    v->addWidget(loginError);
+
+	QFormLayout *form = new QFormLayout;
+    loginUser = new QLineEdit(page);
+    loginUser->setPlaceholderText(tr("Username"));
+    loginPass = new QLineEdit(page);
+    loginPass->setPlaceholderText(tr("Password"));
+    loginPass->setEchoMode(QLineEdit::Password);
+    form->addRow(tr("Username"), loginUser);
+    form->addRow(tr("Password"), loginPass);
+	v->addLayout(form);
+
+    loginSubmit = new QPushButton(tr("Sign In"), page);
+    v->addWidget(loginSubmit);
+
+    connect(loginSubmit, &QPushButton::clicked, this, [this]() {
+        loginError->clear();
+        const QString user = loginUser->text().trimmed();
+        const QString pass = loginPass->text();
+		if (user.isEmpty() || pass.isEmpty()) {
+            loginError->setText(tr("Please enter username and password."));
+			return;
+		}
+		emit loginRequested(user, pass);
+	});
+
+    stack->addWidget(page);
+}
+
+void LogAndRegisterPage::createRegisterPage()
+{
+	QWidget *page = new QWidget(this);
+	QVBoxLayout *v = new QVBoxLayout(page);
+
+    regError = new QLabel(page);
+    regError->setStyleSheet("color: #e11b22");
+    regError->setWordWrap(true);
+    v->addWidget(regError);
+
+	QFormLayout *form = new QFormLayout;
+    regUser = new QLineEdit(page);
+    regUser->setPlaceholderText(tr("Username"));
+    regPass = new QLineEdit(page);
+    regPass->setPlaceholderText(tr("Password"));
+    regPass->setEchoMode(QLineEdit::Password);
+    regPassRepeat = new QLineEdit(page);
+    regPassRepeat->setPlaceholderText(tr("Repeat Password"));
+    regPassRepeat->setEchoMode(QLineEdit::Password);
+
+    form->addRow(tr("Username"), regUser);
+    form->addRow(tr("Password"), regPass);
+    form->addRow(tr("Repeat"), regPassRepeat);
+	v->addLayout(form);
+
+    regSubmit = new QPushButton(tr("Create Account"), page);
+    v->addWidget(regSubmit);
+
+    connect(regSubmit, &QPushButton::clicked, this, [this]() {
+        regError->clear();
+        const QString user = regUser->text().trimmed();
+        const QString pass = regPass->text();
+        const QString pass2 = regPassRepeat->text();
+		if (user.isEmpty() || pass.isEmpty() || pass2.isEmpty()) {
+            regError->setText(tr("All fields are required."));
+			return;
+		}
+		if (pass != pass2) {
+            regError->setText(tr("Passwords do not match."));
+			return;
+		}
+		if (pass.length() < 8) {
+            regError->setText(tr("Password must be at least 8 characters."));
+			return;
+		}
+		emit registerRequested(user, pass);
+	});
+
+    stack->addWidget(page);
+=======
 void LogAndRegisterPage::onLoginClicked()
 {
     const QString user = loginUsername->text().trimmed();
@@ -138,4 +279,5 @@ void LogAndRegisterPage::onRegisterClicked()
     {
         QMessageBox::critical(this, "Register", "Failed to create account. Username may be taken or input invalid.");
     }
+>>>>>>> 5cb0458f0d06901e4cf4ee5770282626e17c14db
 }
--- kino-plex/pages/logandregisterpage.h
+++ kino-plex/pages/logandregisterpage.h
@@ -7,6 +7,11 @@
 
 class QLabel;
 
+class QLineEdit;
+class QPushButton;
+class QLabel;
+class QStackedWidget;
+
 class LogAndRegisterPage : public QWidget
 {
     Q_OBJECT
@@ -15,6 +20,36 @@ public:
     explicit LogAndRegisterPage(QWidget *parent = nullptr);
     ~LogAndRegisterPage();
 
+<<<<<<< HEAD
+signals:
+    void loginRequested(const QString &username, const QString &password);
+    void registerRequested(const QString &username, const QString &password);
+
+private:
+    void createTopBar();
+    void createLoginPage();
+    void createRegisterPage();
+
+    // Top bar
+    QPushButton *signInBtn;
+    QPushButton *registerBtn;
+
+    // Stacked pages
+    QStackedWidget *stack;
+
+    // Login widgets
+    QLineEdit *loginUser;
+    QLineEdit *loginPass;
+    QPushButton *loginSubmit;
+    QLabel *loginError;
+
+    // Register widgets
+    QLineEdit *regUser;
+    QLineEdit *regPass;
+    QLineEdit *regPassRepeat;
+    QPushButton *regSubmit;
+    QLabel *regError;
+=======
 private slots:
     void onLoginClicked();
     void onRegisterClicked();
@@ -32,4 +67,5 @@ private:
     QLineEdit *regPassword = nullptr;
     QLineEdit *regPasswordConfirm = nullptr;
     QPushButton *registerButton = nullptr;
+>>>>>>> 5cb0458f0d06901e4cf4ee5770282626e17c14db
 };
